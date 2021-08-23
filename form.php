af153e1545e4a525f341626e1b209eacfd579015
Edd Pastor <edo.pstr@gmail.com>
2021-08-23 17:50:00 -0500

    Cambios pagina servios y meta tags sharing

--- form.php
+++ form.php
@@ -1,18 +1,20 @@
-<?php
+<?php 
 if(isset($_POST['submit'])){
-    $nombre = $_POST['first_name'];
-    $subject = "Nuevo correo LLDM - test";
-    $correo = $_POST['Email'];
-    $num_contacto = $_POST['num_contacto'];
-    $comentarios = $_POST['comentarios'];
+    $to = "email@example.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-
-    $mailTo = "edo.pstr@gmail.com";
-    $headers = "From: ".$correo;
-    $txt = "Test Email LLDM de ".$nombre.".\n\n".$comentarios;
-
-    mail($mailTo,$subject,$comentarios,$headers);
-    header("Location: index.php?mailsend");
-}
-
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
 ?>
